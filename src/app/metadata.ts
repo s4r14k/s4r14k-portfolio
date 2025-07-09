de153ea76ee9c07b61859cd98324b3dc97c99f4c
s4r14k <sarikraf@gmail.com>
2025-07-09 21:24:08 +0300

    fix favicon.ico

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -5,4 +5,12 @@ export const metadata: Metadata = {
   description: "Personal portfolio showcasing my projects and skills",
   authors: [{ name: "s4r14k" }],
   keywords: ["portfolio", "developer", "software engineer", "projects", "skills"],
+  icons: {
+    icon: [
+      { url: '/favicon.ico', sizes: 'any' },
+      { url: '/logo.png' }
+    ],
+    apple: '/logo.png',
+    shortcut: '/logo.png'
+  },
 };
